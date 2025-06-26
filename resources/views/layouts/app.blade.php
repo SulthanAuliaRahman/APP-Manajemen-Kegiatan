14c18febde8f61f757b0756ed873852e3faf118d
SulthanAuliaRahman <sulthan.aulia.tif23@polban.ac.id>
2025-06-26 23:18:17 +0700

    :bug: solved unrendered tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-indigo-900 text-white">
-    <div class="flex h-screen">
+<body class="bg-indigo-900 text-white flex h-screen">
+    <div class="flex h-full">
         <x-sidebar />
         <div class="flex-1 p-6">
             @yield('content')
